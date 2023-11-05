642246f7650003a830e63ea3c2ad80529e8c5a8c
RomaSarkisyan <sarkisyanrom98@gmail.com>
2023-11-05 22:22:58 +0400

    added homework3

---
+++ homework3.js
@@ -0,0 +1,11 @@
+for (let a = 1; a <= 100; a++) {
+    if (a % 3 === 0 && a % 5 === 0) {
+        console.log("FIZZBUZZ");
+    } else if (a % 3 === 0) {
+        console.log("FIZZ");
+    } else if (a % 5 === 0) {
+        console.log("BUZZ");
+    } else {
+        console.log(a);
+    }
+}
